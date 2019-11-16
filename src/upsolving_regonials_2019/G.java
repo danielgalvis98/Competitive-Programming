db139464cdbf65856741a10329c1999d3eaf60e3
danielgalvis98 <dangaltor@gmail.com>
2019-11-15 23:51:35 -0500

    Testing VS Code

--- src/upsolving_regonials_2019/G.java
+++ src/upsolving_regonials_2019/G.java
@@ -7,35 +7,33 @@ import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class G {
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		String city = br.readLine();
 		int N = Integer.parseInt(br.readLine());
-		
-		SuffixAutomaton suf = new SuffixAutomaton();
-		for (int i = 0; i < suf.maxn; i++) {
-			suf.to[i] = new HashMap<Character, Integer>();
+
+		for (int i = 0; i < SuffixAutomaton.maxn; i++) {
+			SuffixAutomaton.to[i] = new HashMap<Character, Integer>();
 		}
-	
-		suf.addCharacter(city);
-		
+
+		SuffixAutomaton.addCharacter(city);
+
 		for (int cas = 0; cas < N; cas++) {
 			String friend = br.readLine();
 			int count = 0;
 			HashMap<Character, Integer> actNode = new HashMap<Character, Integer>();
 			for (char act : friend.toCharArray()) {
 				if (actNode.containsKey(act)) {
-					actNode = suf.to[actNode.get(act)];
+					actNode = SuffixAutomaton.to[actNode.get(act)];
 				} else {
 					count++;
-					actNode = suf.to[0];
+					actNode = SuffixAutomaton.to[0];
 					if (actNode.containsKey(act)) {
-						actNode = suf.to[actNode.get(act)];
+						actNode = SuffixAutomaton.to[actNode.get(act)];
 					} else {
 						count = -1;
 						break;
@@ -50,21 +48,21 @@ public class G {
 }
 
 class SuffixAutomaton {
-	static final int maxn = 4*100009;
-	static int [] len = new int[maxn];
-	static int [] link = new int[maxn];
-	static int[][] count = new int [2][maxn];
+	static final int maxn = 4 * 100009;
+	static int[] len = new int[maxn];
+	static int[] link = new int[maxn];
+	static int[][] count = new int[2][maxn];
 	static boolean vis[] = new boolean[maxn];
-	
-	static HashMap<Character, Integer> to [] = new HashMap [maxn];
+
+	static HashMap<Character, Integer> to[] = new HashMap [maxn];
 	static int size = 0, last, res;
-	
+
 	public SuffixAutomaton() {
-		link [0] = -1;
+		link[0] = -1;
 		size = 1;
 		last = res = 0;
 	}
-	
+
 	public static void addCharacter(String s) {
 		for (char c : s.toCharArray()) {
 			int p = last;
@@ -79,12 +77,12 @@ class SuffixAutomaton {
 				link[last] = q;
 				continue;
 			}
-			
+
 			int c1 = size++;
 			to[c1].putAll(to[q]);
 			link[c1] = link[q];
 			len[c1] = len[p] + 1;
-			link [last] = link [q] = c1;
+			link[last] = link[q] = c1;
 			for (; p != -1 && to[p].containsKey(c) && to[p].get(c) == q; p = link[p])
 				to[p].put(c, c1);
 		}

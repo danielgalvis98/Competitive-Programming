6768e3d9113aa403c0c003a8d7475ef1dfbfd66c
danielgalvis98 <dangaltor@gmail.com>
2019-11-19 15:33:29 -0500

    E - Antena Coverage from Codeforces Accepted

--- src/codeForces600/antena.java
+++ src/codeForces600/antena.java
@@ -5,10 +5,7 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class antena {
@@ -30,41 +27,23 @@ public class antena {
             lamps.add(new lamp(left, right, center));
         }
 
-        lamps.sort(new Comparator<lamp>() {
-            @Override
-            public int compare(lamp o1, lamp o2) {
-                return o1.center - o2.center;
+        int [] dp = new int [m + 1];
+        dp [m] = 0;
+        for (int i = m - 1; i >= 0; i--){
+            dp[i] = m - i;
+            for (lamp theLamp : lamps){
+                if ( i+1 >= theLamp.left && i+1 <= theLamp.right){
+                    dp [i] = dp [i+1];
+                    break;
+                }
+                if (i < theLamp.left){
+                    int toComplete = theLamp.left - i - 1;
+                    dp [i] = Math.min(dp[i], toComplete + dp[Math.min(m, theLamp.right + toComplete)]);
+                }
             }
-        });
-        
-        int coins = solution(lamps.size() - 1, m);
-
-        bw.write(coins + "\n");
-        bw.close();
-    }
-
-    static int solution(int i, int m) {
-        if (m <= 0)
-            return 0;
-
-        lamp act = lamps.get(i);
-        if (i == 0)
-            return Math.max(Math.max(act.left - 1, m - act.right), 0);
-
-        if (act.right >= m) {
-            m = Math.min(m, act.left - 1);
-            return solution(i - 1, m);
         }
-
-        int op1 = 0;
-        op1 = solution(i - 1, m);
-
-        int op2 = 0;
-        int dif = m - act.right;
-        op2 = dif;
-        op2 += solution(i - 1, act.left - dif - 1);
-
-        return Math.min(op1, op2);
+        bw.write(dp[0] + "\n");
+        bw.close();
     }
 }
 
